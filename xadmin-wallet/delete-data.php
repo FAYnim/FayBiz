f81e2df8d5aecb5b89f34f0a0160a490374e5936
FAYnim <yuana6898@gmail.com>
2025-03-26 17:35:50 +0700

    update delete-data.php - SQL Injection

--- xadmin-wallet/delete-data.php
+++ xadmin-wallet/delete-data.php
@@ -1,5 +1,5 @@
 <?php
-    include "../db-bind.php";
+    include "../db-bind2.php";
     header("Content-Type: application/json");
     $tgl = getWaktuNow();
     $returncode= 200;
@@ -7,16 +7,16 @@
     $id=""; if(isset($_POST["id"])){$id=$_POST["id"];}
 
     $sql = "select * from cv_adminwallet where "
-        ."seq=$id"
+        ."seq = ?"
     ;
-    $table = db_bind($sql);
-    if($table == "empty"){
-        $returncode = 400;
-    } else {
+    $table = db_bind($sql, [$id]);
+    if($table){
         $sql = "delete from cv_adminwallet where "
-            ."seq=$id"
+            ."seq = ?"
         ;
-        db_query($sql);
+        db_bind($sql, [$id]);
+    } else {
+        $returncode = 400;
     }
 
     $r = array(
